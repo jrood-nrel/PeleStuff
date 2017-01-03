92b5bd7b73f3c27e6cda7339ce4f4ccf9755907f
Jon Rood <jrood@nrel.gov>
2017-01-03 15:06:06 -0700

    Updating scripts.

--- mpiperf_pele.sh
+++ mpiperf_pele.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#PBS -N profile_pele
+#PBS -N mpiperf_pele
 #PBS -l nodes=2:ppn=24,walltime=4:00:00
 #PBS -A ExaCT
 #PBS -q short
@@ -13,8 +13,6 @@ set -e
 cd ${HOME}
 
 PELE_ROOT=${HOME}/combustion
-
-source /nopt/intel/16.0/vtune_amplifier_xe/amplxe-vars.sh &> /dev/null
 
 cd ${PELE_ROOT}/PeleC
 git checkout development
@@ -35,11 +33,11 @@ do
   for DIM in 2
   do
     sed -i 's/^max_step.*/max_step = 4000/g' ${PELE_ROOT}/PeleC/Exec/${TEST}/inputs_${DIM}d
-    for COMP_NAME in gnu intel
+    for COMP_NAME in intel
     do
-      for MPI in FALSE TRUE
+      for MPI in TRUE
       do
-        for OMP in FALSE TRUE
+        for OMP in FALSE
         do
           if [ ${COMP_NAME} == 'gnu' ]; then
             #GCC environment
@@ -48,6 +46,7 @@ do
             module load gcc/5.2.0
             module load openmpi-gcc/1.10.0-5.2.0
             module load python/2.7.8
+            source /home/jrood/combustion/mps_standalone/bin/mpsvars.sh
             } &> /dev/null
             COMP_COMMAND=gcc
             FCOMP_COMMAND=gfortran
@@ -55,11 +54,9 @@ do
             #Intel environment
             {
             module purge
-            module load gcc/5.2.0
-            module load compiler/intel/16.0.2
             module load impi-intel/5.1.3-16.0.2 
             module load python/2.7.8
-            module load 
+            source /home/jrood/combustion/mps_standalone/bin/mpsvars.sh
             } &> /dev/null
             COMP_COMMAND=intel
             FCOMP_COMMAND=ifort
@@ -84,10 +81,10 @@ do
 
           if [[ ${MPI} == 'TRUE' ]] && [[ ${OMP} == 'TRUE' ]]; then
             export OMP_NUM_THREADS=6
-            MPI_CMD='mpirun -x OMP_NUM_THREADS -np 8 --map-by ppr:4:node --report-bindings'
+            MPI_CMD='mpirun -mps -np 8'
           elif [[ ${MPI} == 'TRUE' ]] && [[ ${OMP} == 'FALSE' ]]; then
             export OMP_NUM_THREADS=1
-            MPI_CMD='mpirun -np 48 --map-by ppr:24:node --report-bindings'
+            MPI_CMD='mpirun -mps -np 48'
           elif [[ ${MPI} == 'FALSE' ]] && [[ ${OMP} == 'TRUE' ]]; then
             export OMP_NUM_THREADS=24
             MPI_CMD=
@@ -104,15 +101,14 @@ do
           sed -i "s/^DIM.*/DIM        = ${DIM}/g; s/^COMP.*/COMP       = ${COMP_COMMAND}/g; s/^FCOMP.*/FCOMP      = ${FCOMP_COMMAND}/g; s/^USE_MPI.*/USE_MPI    = ${MPI}/g; s/^USE_OMP.*/USE_OMP    = ${OMP}/g" ${PELE_ROOT}/PeleC/Exec/${TEST}/GNUmakefile
           
           printf "Make...\n"
-          make -j 24 &> r001hs-${TEST}-${DIM}d-${COMP_NAME}${OMP_NAME}${MPI_NAME}.make.txt
+          make -j 24 &> ${TEST}-${DIM}d-${COMP_NAME}${OMP_NAME}${MPI_NAME}.make.txt
           printf "Done.\n\n"
           
           printf "Run...\n"
           printf "OMP_NUM_THREADS: $OMP_NUM_THREADS \n"
           set -x
-          amplxe-cl -collect hotspots -result-dir r001hs-${TEST}-${DIM}d-${COMP_NAME}${OMP_NAME}${MPI_NAME} ${MPI_CMD} ./PeleC${DIM}d.${COMP_NAME}${MPI_EXE}${OMP_EXE}.ex inputs_${DIM}d &> r001hs-${TEST}-${DIM}d-${COMP_NAME}${OMP_NAME}${MPI_NAME}.run.txt
-          #amplxe-cl -R summary -result-dir r001hs-${TEST}-${DIM}d-${COMP_NAME}${OMP_NAME}${MPI_NAME} -format=csv &> r001hs-${TEST}-${DIM}d-${COMP_NAME}${OMP_NAME}${MPI_NAME}.profile.summary.txt
-          amplxe-cl -R hotspots -result-dir r001hs-${TEST}-${DIM}d-${COMP_NAME}${OMP_NAME}${MPI_NAME} -format=csv &> r001hs-${TEST}-${DIM}d-${COMP_NAME}${OMP_NAME}${MPI_NAME}.profile.hotspots.txt
+          ${MPI_CMD} ./PeleC${DIM}d.${COMP_NAME}${MPI_EXE}${OMP_EXE}.ex inputs_${DIM}d &> ${TEST}-${DIM}d-${COMP_NAME}${OMP_NAME}${MPI_NAME}.run.txt
+          #mps stat_pele _mps_pele -s -g
           set +x
           printf "Done.\n\n"
           
@@ -120,7 +116,6 @@ do
           {
           rm -r chk*
           rm -r plt*
-          #rm -r r001hs-${TEST}-${DIM}d-${COMP_NAME}${OMP_NAME}${MPI_NAME}
           make clean
           } &> /dev/null
           printf "Done.\n\n"
