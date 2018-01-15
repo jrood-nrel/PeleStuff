adcb57ed6cc7a5a1b6e7236e77a6732838ad7099
Jon Rood <jon.rood@nrel.gov>
2018-01-15 13:43:08 -0700

    Adding verification check for 5pt and 3pt filters.

--- filter/main.cpp
+++ filter/main.cpp
@@ -1,5 +1,6 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<cmath>
 #include "filter.h"
 
 int main(int argc, char *argv[])
@@ -8,13 +9,19 @@ int main(int argc, char *argv[])
   FILE *fr;
   double* OriginalArray;
   double* FilteredArray;
-  const int N = 129;
+  const int N = 65;
   const int x0 = (N-1)/2;
   const int y0 = (N-1)/2;
   const int z0 = (N-1)/2;
   const int r = (N-9)/2;
-  Filter filter = Filter(box_5pt_approx, 2);
+  const int filter_type = box_3pt_approx;
+  Filter filter = Filter(filter_type, 2);
   const int Nf = N-(2*filter.get_filter_ngrow());
+  double norm = 0.0;
+  const double goldNorm5pt = 297.9187131508175526;
+  const double goldNorm3pt = 298.0234308968230152;
+  const double tol = 1e-15;
+  double goldNorm;
 
   OriginalArray = (double*)calloc(N*N*N,sizeof(double));
   FilteredArray = (double*)calloc(Nf*Nf*Nf,sizeof(double));
@@ -42,8 +49,27 @@ int main(int argc, char *argv[])
     }
   }*/
 
+  // Verify results
+  for(int i=0; i < Nf*Nf*Nf; i++){
+    norm += FilteredArray[i] * FilteredArray[i];
+  }
+  norm = sqrt(norm);
+  //printf("%.16f\n",norm);
+
   free(OriginalArray);
   free(FilteredArray);
 
-  return 0;
+  if(filter_type == box_5pt_approx){
+    goldNorm = goldNorm5pt;
+  } else if(filter_type == box_3pt_approx){
+    goldNorm = goldNorm3pt;
+  }
+
+  if(abs(norm-goldNorm) < tol){
+    printf("Success.\n");
+    return 0;
+  } else {
+    printf("Fail.\n");
+    return 1;
+  }
 }
