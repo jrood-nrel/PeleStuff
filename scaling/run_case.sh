cfe1f5d61f2763be7cf3a3f87b910f8b4180ece0
Jon Rood <jon.rood@nrel.gov>
2018-06-04 17:20:05 -0700

    Adding initial script for pelec scaling job submission on cori.

---
+++ scaling/run_case.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+set -e
+
+# Function for printing and executing commands
+cmd() {
+  echo "+ $@"
+  eval "$@"
+}
+
+echo "Running with ${RANKS} ranks and ${THREADS} threads on ${NODES} nodes with a total of ${CORES} cores..."
+
+cmd "export OMP_NUM_THREADS=${THREADS}"
+cmd "export OMP_PLACES=threads"
+cmd "export OMP_PROC_BIND=spread"
+
+(set -x; srun -n ${RANKS} -c ${CORES_PER_RANK} --cpu_bind=cores ${PELEC_EXE} ${INPUT_FILE})
---
+++ scaling/submit_cases_cori.sh
@@ -0,0 +1,64 @@
+#!/bin/bash -l
+
+set -e
+
+# Job settings
+EMAIL="jon.rood@nrel.gov"
+JOB_NAME="pelec-scaling"
+QUEUE="debug"
+CPU_TYPE="haswell"
+ALLOCATION="m2860"
+PELEC_EXE="./PeleC3d.intel.ivybridge.MPI.OMP.ex"
+INPUT_FILE="inputs_3d"
+
+# Create list of jobs with varying parameters to submit
+declare -a JOBS
+#JOBS[x]='nodes:minutes'
+JOBS[0]='1:20'
+JOBS[1]='2:10'
+JOBS[2]='4:5'
+
+# Cori CPU logic
+if [ "${CPU_TYPE}" == 'knl' ]; then
+   #Machine defined
+   CORES_PER_NODE=64
+   HYPERTHREADS=4
+   #User defined
+   RANKS_PER_NODE=16
+   HYPERCORES_PER_THREAD=2
+   CORES_PER_RANK=$((${HYPERTHREADS} * ${CORES_PER_NODE} / ${RANKS_PER_NODE}))
+   EXTRA_ARGS="-S 4"
+elif [ "${CPU_TYPE}" == 'haswell' ]; then
+   #Machine defined
+   CORES_PER_NODE=32
+   HYPERTHREADS=2
+   #User defined
+   RANKS_PER_NODE=8
+   HYPERCORES_PER_THREAD=2
+   CORES_PER_RANK=$((${HYPERTHREADS} * ${CORES_PER_NODE} / ${RANKS_PER_NODE}))
+fi
+
+# Job script submission
+for JOB in "${JOBS[@]}"; do
+   PARAMETER=(${JOB//:/ })
+   NODES=${PARAMETER[0]}
+   JOB_TIME_IN_MINUTES=${PARAMETER[1]}
+   RANKS=$((${NODES} * ${RANKS_PER_NODE}))
+   CORES=$((${CORES_PER_NODE} * ${NODES}))
+   THREADS=$((${CORES_PER_RANK} / ${HYPERCORES_PER_THREAD}))
+   (set -x; echo "sbatch \
+            -A ${ALLOCATION} \
+            -L SCRATCH \
+            -C ${CPU_TYPE} \
+            -J ${JOB_NAME}-${NODES} \
+            -o %x.o%j \
+            -q ${QUEUE} \
+            -N ${NODES} \
+            -t ${JOB_TIME_IN_MINUTES} \
+            --export=NODES=${NODES},RANKS=${RANKS},CORES_PER_RANK=${CORES_PER_RANK},CORES=${CORES},THREADS=${THREADS},PELEC_EXE="${PELEC_EXE}",INPUT_FILE="${INPUT_FILE}" \
+            ${EXTRA_ARGS} \
+            run_case.sh")
+   printf "\n"
+done
+#            --mail-user=${EMAIL} \
+#            --mail-type=ALL \
