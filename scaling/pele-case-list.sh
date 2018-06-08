fc3fbc2c6367ad004adea05c61d3422d161aaae8
Jon Rood <jon.rood@nrel.gov>
2018-06-08 13:43:38 -0700

    Adding more test cases.

--- scaling/pele-case-list.sh
+++ scaling/pele-case-list.sh
@@ -12,16 +12,40 @@ EXAMPLE_JOB='job_name:queue:cpu_type:exe_path:input_file:nodes:ranks_per_node:hy
 declare -a JOBS
 declare -a INPUT_FILE_ARGS
 
-#Cori KNL
-JOBS[1]="pelec-scaling:debug:knl:${OWD}/PeleC3d.${COMPILER}.mic-knl.MPI.ex:${OWD}/input-3d:1:64:2:30"
-INPUT_FILE_ARGS[1]="amr.probin_file=${OWD}/probin-3d amr.n_cell=128 128 128"
-JOBS[2]="pelec-scaling:debug:knl:${OWD}/PeleC3d.${COMPILER}.mic-knl.MPI.ex:${OWD}/input-3d:4:128:2:30"
+#Cori Haswell MPI/OMP Balance
+#JOBS[1]="pelec-scaling:debug:haswell:${OWD}/PeleC3d.${COMPILER}.haswell.MPI.OMP.ex:${OWD}/input-3d:8:32:2:30"
+#INPUT_FILE_ARGS[1]="amr.probin_file=${OWD}/probin-3d amr.n_cell=256 256 256"
+#JOBS[2]="pelec-scaling:debug:haswell:${OWD}/PeleC3d.${COMPILER}.haswell.MPI.OMP.ex:${OWD}/input-3d:8:16:2:30"
+#INPUT_FILE_ARGS[2]="amr.probin_file=${OWD}/probin-3d amr.n_cell=256 256 256"
+#JOBS[3]="pelec-scaling:debug:haswell:${OWD}/PeleC3d.${COMPILER}.haswell.MPI.OMP.ex:${OWD}/input-3d:8:8:2:30"
+#INPUT_FILE_ARGS[3]="amr.probin_file=${OWD}/probin-3d amr.n_cell=256 256 256"
+#JOBS[4]="pelec-scaling:debug:haswell:${OWD}/PeleC3d.${COMPILER}.haswell.MPI.OMP.ex:${OWD}/input-3d:8:4:2:30"
+#INPUT_FILE_ARGS[4]="amr.probin_file=${OWD}/probin-3d amr.n_cell=256 256 256"
+#JOBS[5]="pelec-scaling:debug:haswell:${OWD}/PeleC3d.${COMPILER}.haswell.MPI.OMP.ex:${OWD}/input-3d:8:2:2:30"
+#INPUT_FILE_ARGS[5]="amr.probin_file=${OWD}/probin-3d amr.n_cell=256 256 256"
+
+#Cori KNL MPI/OMP Balance
+JOBS[1]="pelec-scaling:debug:knl:${OWD}/PeleC3d.${COMPILER}.mic-knl.MPI.OMP.ex:${OWD}/input-3d:8:128:2:30"
+INPUT_FILE_ARGS[1]="amr.probin_file=${OWD}/probin-3d amr.n_cell=256 256 256"
+JOBS[2]="pelec-scaling:debug:knl:${OWD}/PeleC3d.${COMPILER}.mic-knl.MPI.OMP.ex:${OWD}/input-3d:8:64:2:30"
 INPUT_FILE_ARGS[2]="amr.probin_file=${OWD}/probin-3d amr.n_cell=256 256 256"
-#JOBS[3]="pelec-scaling:debug:knl:${OWD}/PeleC3d.${COMPILER}.mic-knl.MPI.ex:${OWD}/input-3d:32:128:2:30"
+JOBS[3]="pelec-scaling:debug:knl:${OWD}/PeleC3d.${COMPILER}.mic-knl.MPI.OMP.ex:${OWD}/input-3d:8:32:2:30"
+INPUT_FILE_ARGS[3]="amr.probin_file=${OWD}/probin-3d amr.n_cell=256 256 256"
+JOBS[4]="pelec-scaling:debug:knl:${OWD}/PeleC3d.${COMPILER}.mic-knl.MPI.OMP.ex:${OWD}/input-3d:8:16:2:30"
+INPUT_FILE_ARGS[4]="amr.probin_file=${OWD}/probin-3d amr.n_cell=256 256 256"
+JOBS[5]="pelec-scaling:debug:knl:${OWD}/PeleC3d.${COMPILER}.mic-knl.MPI.OMP.ex:${OWD}/input-3d:8:8:2:30"
+INPUT_FILE_ARGS[5]="amr.probin_file=${OWD}/probin-3d amr.n_cell=256 256 256"
+
+#Cori KNL
+#JOBS[1]="pelec-scaling:debug:knl:${OWD}/PeleC3d.${COMPILER}.mic-knl.MPI.OMP.ex:${OWD}/input-3d:1:16:2:30"
+#INPUT_FILE_ARGS[1]="amr.probin_file=${OWD}/probin-3d amr.n_cell=128 128 128"
+#JOBS[2]="pelec-scaling:debug:knl:${OWD}/PeleC3d.${COMPILER}.mic-knl.MPI.OMP.ex:${OWD}/input-3d:4:32:2:30"
+#INPUT_FILE_ARGS[2]="amr.probin_file=${OWD}/probin-3d amr.n_cell=256 256 256"
+#JOBS[3]="pelec-scaling:debug:knl:${OWD}/PeleC3d.${COMPILER}.mic-knl.MPI.OMP.ex:${OWD}/input-3d:32:32:2:30"
 #INPUT_FILE_ARGS[3]="amr.probin_file=${OWD}/probin-3d amr.n_cell=512 512 512"
-#JOBS[4]="pelec-scaling:debug:knl:${OWD}/PeleC3d.${COMPILER}.mic-knl.MPI.ex:${OWD}/input-3d:256:128:2:30"
+#JOBS[4]="pelec-scaling:debug:knl:${OWD}/PeleC3d.${COMPILER}.mic-knl.MPI.OMP.ex:${OWD}/input-3d:256:32:2:30"
 #INPUT_FILE_ARGS[4]="amr.probin_file=${OWD}/probin-3d amr.n_cell=1024 1024 1024"
-#JOBS[5]="pelec-scaling:regular:knl:${OWD}/PeleC3d.${COMPILER}.mic-knl.MPI.ex:${OWD}/input-3d:2048:128:2:40"
+#JOBS[5]="pelec-scaling:regular:knl:${OWD}/PeleC3d.${COMPILER}.mic-knl.MPI.OMP.ex:${OWD}/input-3d:2048:32:2:40"
 #INPUT_FILE_ARGS[5]="amr.probin_file=${OWD}/probin-3d amr.n_cell=2048 2048 2048"
 
 #Cori Haswell
