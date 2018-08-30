f172b987c583179400679e1567b2902f07ac98b7
Jon Rood <jon.rood@nrel.gov>
2018-08-30 15:21:22 -0600

    Generalizing memory profiling a bit.

--- memory/memory.py
+++ memory/memory.py
@@ -1,13 +1,16 @@
 #!/usr/local/bin/python3
+import pandas as pd
+import matplotlib.pyplot as plt
 
 #Plot memory usage with data collected from ps
 
-import pandas as pd
-import matplotlib.pyplot as plt
 fname = "memory_plot.txt"
+runtime = 1621.239386
+title = "PeleC Memory Usage by MPI Rank - 4 AMR Levels - 1 Step - 1/4 Second Samples"
+
 data = pd.read_csv(fname, usecols=[0,1], header=None, delim_whitespace=True, names=['pid','mem'])
 samples = len(data.values[:,0])
-data['time'] = 1621.239386*(data.index/samples)
+data['time'] = runtime*(data.index/samples)
 data['mem'] = data['mem']/1024
 grouped = data.groupby(['pid'])
 for k, (name, group) in enumerate(grouped):
@@ -15,5 +18,5 @@ for k, (name, group) in enumerate(grouped):
     plt.plot(group.time,group.mem)
 plt.xlabel("Elapsed Run Time (seconds)")
 plt.ylabel("Memory Usage (MB)")
-plt.title("PeleC Memory Usage by MPI Rank - 4 AMR Levels - 1 Step - 1/4 Second Samples")
+plt.title(title)
 plt.show()
--- memory/memory.sh
+++ memory/memory.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Collect memory samples with ps while a proces (mpirun) is running
+
+FILENAME=memory_plot.txt
+
 mpirun -np 16 --oversubscribe ./PeleC3d.gnu.MPI.ex input-3d &
+
 while [[ -n $(jobs -r) ]]; do
-  ps -U jrood -o pid=,rss=,command= | grep Pele | grep -v grep | grep -v mpirun >> memory_plot.txt 2>&1;
-  echo "" >> memory_plot.txt 2>&1;
+  ps -U ${USER} -o pid=,rss=,command= | grep Pele | grep -v grep | grep -v mpirun >> ${FILENAME} 2>&1;
+  echo "" >> ${FILENAME} 2>&1;
   sleep 0.25;
 done
