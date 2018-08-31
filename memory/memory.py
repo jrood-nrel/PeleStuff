e89b941e0cf26caceb70733285116b2932d3865b
Jon Rood <jon.rood@nrel.gov>
2018-08-30 22:06:29 -0600

    Fixing elapsed time axis.

--- memory/memory.py
+++ memory/memory.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 #Plot memory usage with data collected from ps
@@ -7,16 +8,17 @@ import matplotlib.pyplot as plt
 fname = "memory_plot.txt"
 runtime = 1621.239386
 title = "PeleC Memory Usage by MPI Rank - 4 AMR Levels - 1 Step - 1/4 Second Samples"
+num_mpi_ranks = 16
 
 data = pd.read_csv(fname, usecols=[0,1], header=None, delim_whitespace=True, names=['pid','mem'])
-samples = len(data.values[:,0])
-data['time'] = runtime*(data.index/samples)
-data['mem'] = data['mem']/1024
+num_samples = int(len(data)/num_mpi_ranks)
+time = np.arange(0, num_samples)*(runtime/num_samples)
+data['mem'] = data['mem']/1024/1024
 grouped = data.groupby(['pid'])
 for k, (name, group) in enumerate(grouped):
     plt.figure(0)
-    plt.plot(group.time,group.mem)
+    plt.plot(time, group.mem)
 plt.xlabel("Elapsed Run Time (seconds)")
-plt.ylabel("Memory Usage (MB)")
+plt.ylabel("Memory Usage (GB)")
 plt.title(title)
 plt.show()
