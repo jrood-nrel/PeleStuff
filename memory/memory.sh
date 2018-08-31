b480903ea5b5ce75d9b6f429ef79b6e1d3645a2c
Jon Rood <jon.rood@nrel.gov>
2018-08-31 16:34:44 -0600

    Making ps sampling more portable.

--- memory/memory.sh
+++ memory/memory.sh
@@ -7,7 +7,7 @@ FILENAME=memory_plot.txt
 mpirun -np 16 --oversubscribe ./PeleC3d.gnu.MPI.ex input-3d &
 
 while [[ -n $(jobs -r) ]]; do
-  ps -U ${USER} -o pid=,rss=,command= | grep Pele | grep -v grep | grep -v mpirun >> ${FILENAME} 2>&1;
+  ps -U ${USER} -o pid= -o rss= -o command= | grep Pele | grep -v grep | grep -v mpirun | grep -v mpiexec >> ${FILENAME} 2>&1 || true;
   echo "" >> ${FILENAME} 2>&1;
-  sleep 0.25;
+  sleep 0.5;
 done
